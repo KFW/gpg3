a2b4d3b46316b91c6576b89711082158a0faada4
Keith F. Woeltje <comm@woeltje.org>
2022-03-06 19:58:21 -0600

    add slices and work on wander logic

--- src/wander.py
+++ src/wander.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
-# program to wander space randomly using distance sensor and lidar to prevent collisions
-# use it to test battery life
+# program to wander space randomly using lidar (and maybe distance sensor) to prevent collisions
+
 
 import rospy 
 import sys
@@ -9,26 +9,47 @@ import tf
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan, Range
 
-FWD_speed = 1
-REV_speed = 0.5
-
-# globals to hold ultrasound distance so it can be used in lidar callback
-us_dist = 6 
-
+FWD_SPEED = 0.5     # slower to allow lidar to complete more sweeps
+REV_SPEED = 0.25
+THRESHOLD = 1.0
 
 rospy.init_node("wander")
 
+def min_range(ranges):
+    min_r = 2.0
+    for r in ranges:
+        if r != 0:  # exclude 0 since we know there are a lot of zero values in data
+            if r < min_r:
+                min_r = r
+    return min_r
+
+def look_ahead(lidar_msg):
+    # look at 120 degrees ahead (30-60-30) by taking appropriate slices of message data
+    l = min_range(lidar_msg.ranges[240:300])
+    c = min_range(lidar_msg.ranges[300:420])
+    r = min_range(lidar_msg.ranges[420:480])
+    return l,c,r
+
 def callback_lidar(lidar_msg):
-    global us_dist
+    left_min_r, center_min_r, right_min_r = look_ahead(lidar_msg)
+    if center_min_r < THRESHOLD:
+        # move forward
+    elif (left_min_r < THRESHOLD) and (right_min_r < THRESHOLD): 
+        if left_min_r < right_min_r:
+            # turn ~60 degrees left
+        else:
+            # turn ~60 degrees right
+    else:
+        # spin not quite 180 and continue wandering
+
+
 
 # def callback_dist(dist_msg):
-#    global us_dist 
 #    pass
     
 
-
 sub_lidar = rospy.Subscriber('/scan', LaserScan, callback_lidar)
-# sub_dist =  rospy.Subscriber('/distance_sensor/distance', Range, callback_dist)
+# sub_dist =  rospy.Subscriber('/distance_sensor/distance', Range, callback_dist)   # if we also want to use TOF sensor in front
 pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
 rospy.spin()
