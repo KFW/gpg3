5b9eb2891ab4ae21a4034b73e7a345c9fecccb05
Keith F. Woeltje <comm@woeltje.org>
2022-04-18 19:30:11 -0500

    set queue to 1 for scan subscriber; reinstate min ranges

--- src/wander_lidar_test.py
+++ src/wander_lidar_test.py
@@ -11,21 +11,21 @@ from sensor_msgs.msg import LaserScan, Range
 
 rospy.init_node("wander_lidar_test")
 
-# def min_range(ranges):
-#     min_r = 2.0
-#     for r in ranges:
-#         if r != 0:  # exclude 0 since we know there are a lot of zero values in data
-#             if r < min_r:
-#                 min_r = r
-#     return min_r
+def min_range(ranges):
+    min_r = 2.0
+    for r in ranges:
+        if r != 0:  # exclude 0 since we know there are a lot of zero values in data
+            if r < min_r:
+                min_r = r
+    return min_r
 
 def look_ahead(lidar_msg):
-    # for testing look L, C, R
+    # look at 45 degrees ahead (20-5-20 degree slices) by taking appropriate slices of message data
     # lidar reports ranges in 0.5 degree increments; 0 is straight back, 360 straight ahead
-    r = lidar_msg.ranges[180]
-    c = lidar_msg.ranges[360]
-    l = lidar_msg.ranges[540]
-    rospy.loginfo('range L: %.3f  range C: %.3f min R: %.3f' %(l, c, r) )
+    l = min_range(lidar_msg.ranges[315:355])
+    c = min_range(lidar_msg.ranges[355:365])
+    r = min_range(lidar_msg.ranges[365:405])
+    rospy.loginfo('min range L: %.3f  min range C: %.3f min range R: %.3f' %(l, c, r) )
     return l,c,r
 
 # BEGIN CALLBACKS
@@ -40,7 +40,7 @@ def callback_lidar(lidar_msg):
 # END CALLBACKS
     
 # BEGIN SUBSCRIBERS
-sub_lidar = rospy.Subscriber('/scan', LaserScan, callback_lidar)
+sub_lidar = rospy.Subscriber('/scan', LaserScan, callback_lidar, queue_size=1)
 # sub_dist =  rospy.Subscriber('/distance_sensor/distance', Range, callback_dist)   # if we also want to use TOF sensor in front
 # END 
 
